8faeb26dfcd5c647e76f9e95f91d9b246dd8dd77
Andriws102 <Andriws102@gmail.com>
2024-11-08 19:56:06 -0500

    Cambios en vehiculos

--- scriptVehiculos.js
+++ scriptVehiculos.js
@@ -1,14 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
     const vehiculosLista = document.getElementById("vehiculosLista");
 
-    fetch("http://localhost:4000/vehiculos").
-    then(response => response.json())
-    .then(vehiculos => {
-        console.log(vehiculos);
-        vehiculos.forEach(vehiculo => {
-            const li = document.createElement("li");
-            li.textContent = `${vehiculo.placa} - ${vehiculo.marca}`;
-            vehiculosLista.appendChild(li);
-        });
-    })
+    const obtenerVehiculos = () => {
+
+        let elemento = vehiculosLista.querySelector("li");
+        while(elemento !== null){
+            vehiculosLista.removeChild(elemento);
+            elemento = vehiculosLista.querySelector("li");
+        }
+
+        fetch("http://localhost:4000/vehiculos")
+        .then(response => response.json())
+        .then(vehiculos => {
+            vehiculos.forEach(vehiculo => {
+                const li = document.createElement("li");
+                li.textContent = `${vehiculo.placa} - ${vehiculo.marca}`;
+                vehiculosLista.appendChild(li);
+            });
+        })
+    };
+    obtenerVehiculos();
+    setInterval(obtenerVehiculos, 5000);
 });
